caef3aef05514aea4f6f5a5bcdd5e810ee0c37f7
Andrew Ellington <aellington8567@gmail.com>
2023-05-04 17:42:50 -0400

    test code for hasing password

--- geocoding-test.js
+++ geocoding-test.js
@@ -27,3 +27,23 @@ const req = https.request(options, (res) => {
 }).on("error", (err) => {
     console.log("Error: ", err)
 }).end()
+var testpassword='test';
+const store=testpassword;
+var test2 = 'dsfkjlewkljfdjldk';
+var testHash = '$2b$10$ukEIWEVm1uMt6dFerCWIXuV5TlEvWYjBjD6PETnz4HNsDGIoCxBDS';
+bcrypt.genSalt(10,  (err, salt) => {
+  //  console.log(salt);
+})
+async function hashPassword(testpassword) {
+    const hash = await bcrypt.hash(testpassword, 10);
+    // Store hash in the database
+    console.log(hash);
+}
+async function comparePassword(plaintextPassword, hash) {
+    const result = await bcrypt.compare(plaintextPassword, hash);
+    console.log(result);
+    return result;
+    
+}
+hashPassword(testpassword);
+comparePassword(testpassword,testHash);
